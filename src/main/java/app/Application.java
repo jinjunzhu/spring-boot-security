a30b7d6297d1616d6c7a7a9098ed0201f970357e
jinjunzhu <zjj2006forever@163.com>
2020-05-06 13:19:47 +0800

    增加actuator测试

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@SpringBootApplication
-@ComponentScan("app,repository,service,domain")
+@SpringBootApplication(scanBasePackages={"app", "repository", "service","domain"})
 public class Application {
 
 	public static void main(String[] args) throws Throwable {
--- src/test/java/boot/service/UserServiceTest.java
+++ src/test/java/boot/service/UserServiceTest.java
@@ -2,14 +2,14 @@ package boot.service;
 
 import javax.annotation.Resource;
 
+import boot.support.AbstractSpringbootTest;
 import org.junit.Test;
 
-import boot.support.SpringTxTestCase;
 import domain.MyUser;
 import service.UserService;
 
 
-public class UserServiceTest extends SpringTxTestCase{
+public class UserServiceTest extends AbstractSpringbootTest {
 	
 	@Resource
 	private UserService userService;
